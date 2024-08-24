80b2fa95d46bdea794fac1d085a109f0e95e762a
ZakTan <Ztanksley@luc.edu>
2024-08-24 12:36:57 -0700

    renamed variables, implemented `RulesDesriptionLabel` with constrants

--- CardWorkout_Programmatic/RulesVC.swift
+++ CardWorkout_Programmatic/RulesVC.swift
@@ -8,22 +8,47 @@
 import UIKit
 
 class RulesVC: UIViewController {
-
+    
+    let rulesTitleLabel = UILabel()
+    let rulesDescriptionLabel = UILabel()
+    let activityInfoLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureRulesTileLabel()
+        configureRulesDesriptionLabel()
 
-        // Do any additional setup after loading the view.
+        
+    }
+    func configureRulesTileLabel() {
+        view.addSubview(rulesTitleLabel)
+        rulesTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        rulesTitleLabel.text = "Rules"
+        rulesTitleLabel.font = .systemFont(ofSize: 40, weight: .bold)
+        rulesTitleLabel.textAlignment = .center
+        
+        NSLayoutConstraint.activate([
+            rulesTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 70),
+            rulesTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            rulesTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureRulesDesriptionLabel() {
+        view.addSubview(rulesDescriptionLabel)
+       rulesDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+       rulesDescriptionLabel.text = "The value of each card represents the value of excercise you do.\n\n J = 11, K = 12, Q = 13, A = 14"
+        rulesDescriptionLabel.font = .systemFont(ofSize: 25, weight: .semibold)
+        rulesDescriptionLabel.textColor = .red
+        rulesDescriptionLabel.textAlignment = .center
+        rulesDescriptionLabel.lineBreakMode = .byWordWrapping
+        rulesDescriptionLabel.numberOfLines = 0
+        
+        NSLayoutConstraint.activate([
+            rulesDescriptionLabel.topAnchor.constraint(equalTo: rulesTitleLabel.bottomAnchor, constant: 20),
+            rulesDescriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            rulesDescriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
     }
-    */
-
 }
